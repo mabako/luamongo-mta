ceef8bdb0242648400e9433f36f0c124989ccc9e
mabako <mabako@gmail.com>
2012-02-05 15:29:03 +0100

    Initial commit that has only been tested on Windows.

---
+++ ml_base.cpp
@@ -0,0 +1,51 @@
+/*********************************************************
+*
+*  Multi Theft Auto: San Andreas - Deathmatch
+*
+*  ml_base, External lua add-on module
+*  
+*  Copyright © 2003-2008 MTA.  All Rights Reserved.
+*
+*  Grand Theft Auto is © 2002-2003 Rockstar North
+*
+*  THE FOLLOWING SOURCES ARE PART OF THE MULTI THEFT
+*  AUTO SOFTWARE DEVELOPMENT KIT AND ARE RELEASED AS
+*  OPEN SOURCE FILES. THESE FILES MAY BE USED AS LONG
+*  AS THE DEVELOPER AGREES TO THE LICENSE THAT IS
+*  PROVIDED WITH THIS PACKAGE.
+*
+*********************************************************/
+
+#include "ml_base.h"
+
+extern "C" { int luaopen_mongo(lua_State *L); }
+
+
+// Initialisation function (module entrypoint)
+MTAEXPORT bool InitModule ( void *pManager, char *szModuleName, char *szAuthor, float *fVersion )
+{
+    // Set the module info
+    strncpy ( szModuleName, MODULE_NAME, MAX_INFO_LENGTH );
+    strncpy ( szAuthor, MODULE_AUTHOR, MAX_INFO_LENGTH );
+    (*fVersion) = MODULE_VERSION;
+
+    return true;
+}
+
+
+MTAEXPORT void RegisterFunctions ( lua_State * luaVM )
+{
+	luaopen_mongo( luaVM );
+}
+
+
+MTAEXPORT bool DoPulse ( void )
+{
+    return true;
+}
+
+MTAEXPORT bool ShutdownModule ( void )
+{
+    
+    return true;
+}
