824973a8c5480652effbfbf44290ce69419796a6
mabako <mabako@gmail.com>
2012-03-31 02:53:10 +0200

    Base on commit 8e22adc8, increased Version to 4
    Simplified versions actually, from 1.0 (1) -> 1.01 (2) -> 1.02 (3) -> 4th release now.

--- ml_base.h
+++ ml_base.h
@@ -2,9 +2,9 @@
 // see LICENSE for details.
 
 /** MODULE SPECIFIC INFORMATION **/
-#define MODULE_NAME         "MongoDB Database Adapter"
+#define MODULE_NAME         "MongoDB Database Adapter/luamongo(8e22adc8)"
 #define MODULE_AUTHOR       "mabako"
-#define MODULE_VERSION      1.02
+#define MODULE_VERSION      4
 
 #include <string.h>
 #include "Common.h"
